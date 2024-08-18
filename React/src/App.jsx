80ba9a5f07705f1d28b5b4096c6ec2bda363777f
OnShubham <shubhamkharche3005@gmail.com>
2024-08-18 10:37:34 +0530

    add a ShadCn

---
+++ React/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="m-6">
+      <Button> hello</Button>
+    </div>
+  );
+}
+
+export default App;
