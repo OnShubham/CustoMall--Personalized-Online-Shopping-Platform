37e181406ab5b3f0b3bcc65216001a2f2a0a92bf
OnShubham <shubhamkharche3005@gmail.com>
2024-08-19 23:47:51 +0530

    ad

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,34 @@
-import { Button } from "./components/ui/button";
+import React from "react";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import Home from "./page/home";
+import Login from "./page/login";
+import Footer from "./components/home/footer";
+import { NavigationMenuDemo } from "./components/home/navigation";
+// Define your routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <div className="m-6">
-      <Button> hellod</Button>
+    <div className="flex flex-col min-h-screen">
+      {/* Navigation stays at the top */}
+      <Navigation />
+
+      {/* Main content in a container */}
+      <main className="flex-grow container mx-auto p-6">
+        <RouterProvider router={router} />
+      </main>
+
+      {/* Footer stays at the bottom */}
+      <Footer />
     </div>
   );
 }
